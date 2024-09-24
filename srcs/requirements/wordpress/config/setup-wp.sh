1f06648da96daea0b18bb9a39a6b48bdb59cc26a
Pablo Rando Alcalá <prando-a@student.42malaga.com>
2024-09-24 17:25:17 +0200

    dshwudh

--- srcs/requirements/wordpress/config/setup-wp.sh
+++ srcs/requirements/wordpress/config/setup-wp.sh
@@ -3,10 +3,9 @@
 # Espera a que MariaDB esté listo
 
 # Crea el archivo wp-config.php si no existe
-if [ ! -f /var/www/html/wp-config.php ]; then
-    echo "Creando wp-config.php..."
-    wp config create --path=/var/www/html --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_NAME} --allow-root
-fi
+echo "Creando wp-config.php..."
+wp config create --path=/var/www/html --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_NAME} --allow-root
+
 
 # Inicia PHP-FPM
 echo "Iniciando PHP-FPM..."
