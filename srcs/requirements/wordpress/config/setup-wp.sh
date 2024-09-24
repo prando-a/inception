a933f95d09aa11f35816823069f023a80f762848
Pablo Rando Alcalá <prando-a@student.42malaga.com>
2024-09-24 14:46:00 +0200

    wp mi dolor

--- srcs/requirements/wordpress/config/setup-wp.sh
+++ srcs/requirements/wordpress/config/setup-wp.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
 
-# Load environment variables from .env file
-export $(grep -v '^#' /root/.env | xargs)
+# Espera a que MariaDB esté listo
+until mysql -h"${WP_DB_HOST}" -u"${WP_DB_USER}" -p"${WP_DB_PASS}" -e 'show databases;' > /dev/null 2>&1; do
+    echo "Esperando a que MariaDB esté listo..."
+    sleep 5
+done
 
-# Set the path where WordPress should be installed
-WP_PATH="/var/www/html"
-
-# Download WordPress if not already present
-if [ ! -f "$WP_PATH/wp-config.php" ]; then
-    echo "Downloading WordPress..."
-    wp core download --path=$WP_PATH --allow-root
+# Crea el archivo wp-config.php si no existe
+if [ ! -f /var/www/html/wp-config.php ]; then
+    echo "Creando wp-config.php..."
+    wp config create --path=/var/www/html --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS} --dbhost=${WP_DB_HOST} --allow-root
 fi
 
-# Create the wp-config.php file using the environment variables
-echo "Configuring WordPress..."
-wp core config --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --path=$WP_PATH --allow-root
+# Inicia PHP-FPM
+echo "Iniciando PHP-FPM..."
+php-fpm7.4 -F
 
-# Install WordPress core
-echo "Installing WordPress..."
-wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path=$WP_PATH --allow-root
-
-# Change ownership and permissions
-echo "Setting permissions..."
-usermod -aG wp_group $(whoami)
-chown -R $(whoami):wp_group $WP_PATH && chmod -R 775 $WP_PATH
-
-# Start PHP-FPM
-echo "Starting PHP-FPM..."
-php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
-
-echo "WordPress installation and configuration complete!"
