542a44b4c84a5cde9016ce91ab4911000d986403
Pablo Rando Alcalá <prando-a@student.42malaga.com>
2024-09-24 17:33:18 +0200

    dshwudh

--- srcs/requirements/wordpress/config/setup-wp.sh
+++ srcs/requirements/wordpress/config/setup-wp.sh
@@ -7,7 +7,10 @@ echo "Creando wp-config.php..."
 wp config create --path=/var/www/html --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_NAME} --allow-root
 
 
+echo "Instalando WordPress..."
+wp core install --path=/var/www/html --url=${DOMAIN_NAME} --title=${hola} --admin_user=${DB_USER} --admin_password=${DB_PASS} --admin_email=${prando-a@student.42malaga.com} --allow-root
+
 # Inicia PHP-FPM
 echo "Iniciando PHP-FPM..."
-php-fpm7.4 -F
+php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F 
 
