4f41b453966c4fc78108a8ebb42b18cd76001954
Pablo Rando Alcalá <prando-a@student.42malaga.com>
2024-09-24 17:39:11 +0200

    aiuda

--- srcs/requirements/wordpress/config/setup-wp.sh
+++ srcs/requirements/wordpress/config/setup-wp.sh
@@ -4,6 +4,7 @@
 
 # Crea el archivo wp-config.php si no existe
 echo "Creando wp-config.php..."
+rm -rf /var/www/html/wp-config.php
 wp config create --path=/var/www/html --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_NAME} --allow-root
 
 
