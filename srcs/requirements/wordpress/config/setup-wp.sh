e72e96fd539164f9ac78a73c56d72b7646d2bb84
prando-a <prando-a@student.42malaga.com>
2024-09-26 20:12:00 +0200

    patata

--- srcs/requirements/wordpress/config/setup-wp.sh
+++ srcs/requirements/wordpress/config/setup-wp.sh
@@ -8,14 +8,6 @@ chmod -R 755 /var/www/html/wordpress
 
 sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = wordpress:9000#g' /etc/php/7.4/fpm/pool.d/www.conf
 
-mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-
-sed -i "s/database_name_here/$MARIA_DATABASE/" /var/www/html/wordpress/wp-config.php
-sed -i "s/username_here/$MARIA_USER/" /var/www/html/wordpress/wp-config.php
-sed -i "s/password_here/$MARIA_PASS/" /var/www/html/wordpress/wp-config.php
-sed -i "s/put your unique phrase here/$PHRASE/" /var/www/html/wordpress/wp-config.php
-sed -i "s/localhost/mariadb:3306/" /var/www/html/wordpress/wp-config.php
-
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
@@ -28,10 +20,10 @@ fi
 
 mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 
-sed -i "s/database_name_here/$MARIA_DATABASE/" /var/www/html/wordpress/wp-config.php
-sed -i "s/username_here/$MARIA_USER/" /var/www/html/wordpress/wp-config.php
-sed -i "s/password_here/$MARIA_PASS/" /var/www/html/wordpress/wp-config.php
-sed -i "s/put your unique phrase here/$PHRASE/" /var/www/html/wordpress/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$DB_USER/" /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$DB_ROOT_PASS/" /var/www/html/wordpress/wp-config.php
+sed -i "s/put your unique phrase here/trying/" /var/www/html/wordpress/wp-config.php
 sed -i "s/localhost/mariadb:3306/" /var/www/html/wordpress/wp-config.php
 
 wp core install --allow-root --url=$DB_NAME --title=NekaWeb --admin_user=$DB_USER --admin_password=$DB_ROOT_PASS --admin_email=$WP_ADMIN_MAIL --skip-email --path=/var/www/html/wordpress
