6a09fb8e88448a474901219a45c2e0093d8098c9
prando-a <prando-a@student.42malaga.com>
2024-09-27 16:17:50 +0200

    jaja

--- srcs/requirements/mariadb/tools/setup-db.sh
+++ srcs/requirements/mariadb/tools/setup-db.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+echo "[DEBUG]: env passed..."
+echo "--------------------------"
 echo "NAME: $DB_NAME"
 echo "USERNAME: $DB_USER"
 echo "PASSWORD: $DB_PASS"
 echo "ROOT PASSWORD: $DB_ROOT_PASS"
+echo "--------------------------"
 
 service mariadb start && \
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > msql_db.sql && \
@@ -13,7 +16,11 @@ echo "FLUSH PRIVILEGES;" >> msql_db.sql && \
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}' ;" >> msql_db.sql && \
 mariadb < msql_db.sql
 
+
+echo "[DEBUB]: msql_db.sql content:"
+echo "--------------------------"
 cat msql_db.sql
+echo "--------------------------"
 
 chmod 777 msql_db.sql
 mv msql_db.sql /run/mysqld/msql_db.sql
