d828da00f770dfbea8db0db04fcab66a8b69fdf5
prando-a <prando-a@student.42malaga.com>
2024-09-27 14:19:38 +0200

    jaja

---
+++ srcs/requirements/mariadb/tools/setup-db.sh
@@ -0,0 +1,13 @@
+service mariadb start && \
+echo "CREATE DATABASE IF NOT EXISTS mariadbs ;" > msql_db.sql && \
+echo "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}' ;" >> msql_db.sql && \
+echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' ;" >> msql_db.sql && \
+echo "FLUSH PRIVILEGES;" >> msql_db.sql && \
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}' ;" >> msql_db.sql && \
+mariadb < msql_db.sql
+
+chmod 777 msql_db.sql
+mv msql_db.sql /run/mysqld/msql_db.sql
+chown -R mysql:root /var/run/mysqld
+
+mariadbd --user=mysql --init-file=/run/mysqld/msql_db.sql
