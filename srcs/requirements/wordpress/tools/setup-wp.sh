97ad536a6fad16f16d06035edcf544f5c86cc9ad
Pablo Rando Alcala <prando-a@c3r2s5.42malaga.com>
2024-09-30 19:46:47 +0200

    aa

--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /run/php
 
-chown -R www-data.www-data /var/www/html/wordpress
+chown -R www-data.www-data /var/www/html
 
 chmod -R 755 /var/www/html/wordpress
 
@@ -12,20 +12,20 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
 
-if [ ! -f /var/www/html/wordpress/wp-load.php ]; then
-    wp core download --allow-root --path=/var/www/html/wordpress
+if [ ! -f /var/www/html/wp-load.php ]; then
+    wp core download --allow-root --path=/var/www/html
 fi
 
-mv ./wp-config.php /var/www/html/wordpress/wp-config.php
+mv ./wp-config.php /var/www/html/wp-config.php
 
-echo "$DB_NAME\t$DB_USER\t$DB_ROOT_PASS\t$WP_ADMIN_MAIL FINISH"
+echo "$DB_NAME\t$DB_USER\t$DB_PASS\t$WP_ADMIN_MAIL FINISH"
 
-sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wordpress/wp-config.php
-sed -i "s/username_here/$DB_USER/" /var/www/html/wordpress/wp-config.php
-sed -i "s/password_here/$DB_ROOT_PASS/" /var/www/html/wordpress/wp-config.php
-sed -i "s/localhost/$DB_NAME/" /var/www/html/wordpress/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
+sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
+sed -i "s/password_here/$DB_PASS/" /var/www/html/wp-config.php
+sed -i "s/localhost/$DB_NAME/" /var/www/html/wp-config.php
 
-wp core install --allow-root --url=$DB_NAME --title=inception --admin_user=$DB_USER --admin_password=$DB_ROOT_PASS --admin_email=$WP_ADMIN_MAIL --skip-email --path=/var/www/html/wordpress
+wp core install --allow-root --url=$DOMAIN_NAME --title=inception --admin_user=$DB_USER --admin_password=$DB_ROOT_PASS --admin_email=$WP_ADMIN_MAIL --path=/var/www/html
 
 chown -R prando-a:wp_group /var/www/html && chmod -R 775 /var/www/html
 
