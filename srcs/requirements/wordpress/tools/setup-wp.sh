971596ee12aa459ebc1224051dddebf66cb72d7f
prando-a <prando-a@student.42malaga.com>
2024-10-01 01:49:34 +0200

    wa

--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -10,11 +10,6 @@ sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
 sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
 sed -i "s/password_here/$DB_PASS/g" /var/www/html/wp-config.php
 sed -i "s/localhost/$DB_NAME/g" /var/www/html/wp-config.php
-wp core install --allow-root --path=/var/www/html \
-	--url=$DOMAIN_NAME --title=inception \
-	--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS \
-	--admin_email=$WP_ADMIN_MAIL
-wp user create $DB_USER $WP_USER_MAIL --user_pass=$DB_PASS --allow-root --role=author --path=/var/www/html
 
 chown -R wp_user:wp_group /var/www/html && chmod -R 775 /var/www/html
 php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
