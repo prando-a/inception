70ee6618262658c0574fd836916942dfb5bf9c83
Pablo Rando Alcala <prando-a@c3r2s5.42malaga.com>
2024-09-30 20:19:50 +0200

    aa

--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -6,7 +6,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root --path=/var/www/html
-cp /scripts/wp-config.php /var/www/html/wp-config.php
+cp wp-config.php /var/www/html/wp-config.php
 
 sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
 sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
