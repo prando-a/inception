374aaa6689d376d1238bc9412f3e97be76aa3aa0
Pablo Rando Alcala <prando-a@c3r2s5.42malaga.com>
2024-09-30 20:11:10 +0200

    aa

--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -1,27 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+if [ ! -f /var/www/html/wp-config.php ]; then
 
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-
 mv wp-cli.phar /usr/local/bin/wp
+wp core download --allow-root --path=/var/www/html
+cp /scripts/wp-config.php /var/www/html/wp-config.php
 
-if [ ! -f /var/www/html/wp-load.php ]; then
-    wp core download --allow-root --path=/var/www/html
+sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
+sed -i "s/password_here/$DB_PASS/g" /var/www/html/wp-config.php
+sed -i "s/localhost/$DB_NAME/g" /var/www/html/wp-config.php
+wp core install --allow-root --path=/var/www/html \
+	--url=$DOMAIN_NAME --title=inception \
+	--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS \
+	--admin_email=$WP_ADMIN_MAIL
+wp user create $DB_USER $WP_USER_MAIL --user_pass=$DB_PASS --allow-root --role=author --path=/var/www/html
+
 fi
 
-mv ./wp-config.php /var/www/html/wp-config.php
-
-echo "$DB_NAME\t$DB_USER\t$DB_PASS\t$WP_ADMIN_MAIL FINISH"
-
-sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
-sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
-sed -i "s/password_here/$DB_PASS/" /var/www/html/wp-config.php
-sed -i "s/localhost/$DB_NAME/" /var/www/html/wp-config.php
-
-wp core install --allow-root --url=$DOMAIN_NAME --title=inception --admin_user=$DB_USER --admin_password=$DB_ROOT_PASS --admin_email=$WP_ADMIN_MAIL --path=/var/www/html
-
-chmod -R 755 /var/www/html
 chown -R wp_user:wp_group /var/www/html && chmod -R 775 /var/www/html
-
 php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
