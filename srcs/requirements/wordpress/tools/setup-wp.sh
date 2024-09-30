9e1628761b22c14187547341aa79b370546c6087
prando-a <prando-a@student.42malaga.com>
2024-10-01 01:54:45 +0200

    wa

--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -12,3 +12,5 @@ sed -i "s/localhost/$DB_NAME/g" /var/www/html/wp-config.php
 
 chown -R wp_user:wp_group /var/www/html && chmod -R 775 /var/www/html
 php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
+
+cat /scripts/title.txt
