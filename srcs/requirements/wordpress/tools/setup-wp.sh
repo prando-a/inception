398b64d41fd011524a772937de9057554ea4f514
Pablo Rando Alcala <prando-a@c3r8s2.42malaga.com>
2024-10-01 16:18:16 +0200

    eoo

--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
@@ -17,4 +23,9 @@ wp core install --allow-root --path=/var/www/html \
 wp user create $DB_USER $WP_USER_MAIL --user_pass=$DB_PASS --allow-root --role=author --path=/var/www/html
 
 chown -R wp_user:wp_group /var/www/html && chmod -R 775 /var/www/html
+
+echo  -e "${YELLOW} Dedicated to my beloved L"
+cat /scripts/title.txt
+echo -e "${RED}	Freshly baked with blood\n\t\t${RED}and pain ! Sweet!!!"
+
 php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
